bccaf188e9e989a79b0680888cab5e614dd9cb35
Aniruddha Sawant <andysawant@gmail.com>
2020-09-08 21:37:34 +1000

    Swagger spec file added

--- src/main/java/com/sawant/employeeinformation/config/EmployeeRoutingConfig.java
+++ src/main/java/com/sawant/employeeinformation/config/EmployeeRoutingConfig.java
@@ -1,7 +1,6 @@
 package com.sawant.employeeinformation.config;
 
 import com.sawant.employeeinformation.service.EmployeeService;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-
 import static org.springframework.web.reactive.function.BodyInserters.fromValue;
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Slf4j
 @Configuration
@@ -46,35 +44,35 @@ public class EmployeeRoutingConfig {
     @Bean
     public RouterFunction<ServerResponse> getAllEmployees(EmployeeService employeeService){
         return RouterFunctions.route(GET(getAllEmployees), request ->{
-            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Welcome"));
+            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Get All Employees"));
         });
     }
 
     @Bean
     public RouterFunction<ServerResponse> getEmployeeById(EmployeeService employeeService){
         return RouterFunctions.route(GET(getEmployeeById), request ->{
-            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Welcome"));
+            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Get Employee by Id"));
         });
     }
 
     @Bean
     public RouterFunction<ServerResponse> addEmployee(EmployeeService employeeService){
-        return RouterFunctions.route(GET(addEmployee), request ->{
-            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Welcome"));
+        return RouterFunctions.route(POST(addEmployee), request ->{
+            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Add Employee"));
         });
     }
 
     @Bean
     public RouterFunction<ServerResponse> updateEmployee(EmployeeService employeeService){
-        return RouterFunctions.route(GET(updateEmployee), request ->{
-            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Welcome"));
+        return RouterFunctions.route(PUT(updateEmployee), request ->{
+            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Update Employee"));
         });
     }
 
     @Bean
     public RouterFunction<ServerResponse> deleteEmployee(EmployeeService employeeService){
-        return RouterFunctions.route(GET(deleteEmployee), request ->{
-            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Welcome"));
+        return RouterFunctions.route(DELETE(deleteEmployee), request ->{
+            return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromValue("Delete Employee"));
         });
     }
 
